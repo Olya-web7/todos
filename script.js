1c76079249188eb1ad46a47f2398427c707f8672
Olya-web7 <79056080+Olya-web7@users.noreply.github.com>
2021-02-19 21:38:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+  todos.forEach(todo => addTodo(todo))
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
   addTodo();
 })
 
@@ -21,20 +26,40 @@ function addTodo(todo) {
       todoEl.classList.add('completed');
     }
 
-    todoEl.innerText = todoText
+    todoEl.innerText = todoText;
 
-    todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'));
+    todoEl.addEventListener('click', () => {
+      todoEl.classList.toggle('completed');
+      updateLS();
+    });
 
     todoEl.addEventListener('contextmenu', (e) => {
         e.preventDefault();
 
         todoEl.remove();
+        updateLS();
     })
 
     todosUL.appendChild(todoEl);
-
-    input.value = ''
+    input.value = '';
+    updateLS();
   }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll('li');
+  const todos = [];
+
+  todosEl.forEach(todoEl => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains('completed')
+    })
+
+  })
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+
 }
 
 
